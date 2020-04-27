0df055e3e0afd23ef106c932b09acce0b5d78c0e
DavidColatti <davidcolatti@gmail.com>
2020-04-27 15:16:37 -0400

    spotify api attempt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import SpotifyWebApi from 'spotify-web-api-node';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-     Hello its me
-      </header>
-      HIIII Hows It Going?
-    </div>
-  );
+// credentials are optional
+let spotifyApi = new SpotifyWebApi({
+  clientId: 'c29104c04c35403a821d715d9f41b734',
+  clientSecret: 'b8a9047faedd438fa41b3479b735bea3',
+  redirectUri: 'http://localhost:3000/callback'
+});
+
+spotifyApi.setAccessToken('BQABLab-BF9E0FrPM6ACZnzRg1WupDDXWCSOFCtiNTF1schKk5x9mw7-hZcVs_K7kiFkXg5MiSZ2bjMBAcCcTZ54yGIWkTMPoSN4ZopKpOi7YzpZ2Vn_KDImnf5PWKFoV3g3PV5FsbIxAT3cLNymZl5Rui1uLEGbPaM');
+
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+  function(data) {
+    console.log('Artist albums', data.body);
+  },
+  function(err) {
+    console.error(err);
+  }
+);
+
+class App extends Component {
+  async componentDidMount() {
+    
+  }
+
+	render() {
+		return <div />;
+	}
 }
 
 export default App;
