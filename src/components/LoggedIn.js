ed257ecfc66398039e9cbed69530e634ad5a4cca
DavidColatti <davidcolatti@gmail.com>
2020-04-28 16:08:22 -0400

    buttons works for loggedin

--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -2,29 +2,57 @@ import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 
 class LoggedIn extends Component {
+	state = {
+		displayHome: true,
+		displayScenes: false,
+		displayMeditate: false,
+		displaySleep: false
+	};
+
 	makeNavBar = () => {
 		return (
 			<nav>
 				<ul>
 					<li>
-						<button>Home</button>
+						<button onClick={() => this.setState({ displayHome: !this.state.displayHome })}>Home</button>
 					</li>
 					<li>
-						<button>Scenes</button>
+						<button onClick={() => this.setState({ displayScenes: !this.state.displayScenes })}>
+							Scenes
+						</button>
 					</li>
 					<li>
-						<button>Meditate</button>
+						<button onClick={() => this.setState({ displayMeditate: !this.state.displayMeditate })}>
+							Meditate
+						</button>
 					</li>
 					<li>
-						<button>Sleep</button>
+						<button onClick={() => this.setState({ displaySleep: !this.state.displaySleep })}>Sleep</button>
 					</li>
 				</ul>
 			</nav>
 		);
 	};
 
+	displayContent = () => {
+		if (this.state.displayHome) {
+			return <div>DISPLAYING HOME</div>;
+		} else if (this.state.displayScenes) {
+			return <div>DISPLAYING SCENES</div>;
+		} else if (this.state.displayMeditate) {
+			return <div>DISPLAYING MEDITATE</div>;
+		} else if (this.state.displaySleep) {
+			return <div>DISPLAYING SLEEP</div>;
+		}
+	};
+
 	render() {
-		return <div>{this.makeNavBar()}</div>;
+		return (
+			<div>
+				<div>{this.makeNavBar()}</div>
+				<div>{this.displayContent()}</div>
+			</div>
+		);
 	}
 }
 
