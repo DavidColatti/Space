6ba0184e38f81bd0483c049c0fd55bc61f77583c
gabrielacarr <briecardo@icloud.com>
2020-04-29 09:37:20 -0400

    videoapp

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+			<video autoPlay loop id="videobackground" contols="false">
+					<source src="meadow.mp4" type="video/mp4" />
+				</video>
 				<Switch>
 					<Route exact path="/" render={(props) => <Home {...props} />} />
 				</Switch>
