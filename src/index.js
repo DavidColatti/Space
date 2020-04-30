3bc5b82776807a949770fde4b999113894f61ceb
DavidColatti <davidcolatti@gmail.com>
2020-04-30 09:57:53 -0400

    pulled

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import '@fortawesome/fontawesome-free/css/all.min.css'; import
-'bootstrap-css-only/css/bootstrap.min.css'; import
-'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import './components/Home.css';
 
