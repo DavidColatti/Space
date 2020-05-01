de5e01ae1d72ae365abaca5b40d79713e3bbb7b4
gabrielacarr <briecardo@icloud.com>
2020-05-01 16:36:57 -0400

    shortbio

--- src/components/MeditateDetails.js
+++ src/components/MeditateDetails.js
@@ -25,31 +25,34 @@ class MeditateDetails extends Component {
     });
   }
 
-  getNarrators = () => {
+
+  getNarrators = () => { // NARRATOR FUNCTION- COLLECT HEADSHOT, NAME, AND TITLE
     let narrators = this.state.program.narrators.splice(0, 1);
     console.log(narrators);
 
     return narrators.map((each) => {
       return (
-        <div>
+        <div className="narrator">
 
-          <div className="img-name-title">
-            <img className="headshot" src={each.headshot.url} alt="headshotimage"/>
-            <h3 className="eachName">{each.name}</h3>
-            <h3 className="eachTitle" >{each.display_title}</h3>
+          <div className="narrator-img-name-title">
+            <img className="headshot" src={each.headshot.url} alt="headshotimage" />
+            <h3 className="narrator-name"> {each.name} </h3>
+            <h3 className="narrator-title" > {each.display_title} </h3>
 
           </div>
 
-          <div>
-            <h4>{each.short_bio}</h4>
-            <h6>{each.bio}</h6>
+          <div className="bio">
+
+            <h4 className="narrator-short-bio">{each.short_bio}</h4>
+            <h6 className="narrator-bio" > {each.bio}</h6>
+
           </div>
         </div>
       );
     });
   };
 
-  getGuides = () => {
+  getGuides = () => { // GUIDE FUNCTION- TITLE AND AUDIO 
     let guides = this.state.program.guides;
     console.log(guides);
 
@@ -57,7 +60,7 @@ class MeditateDetails extends Component {
       return (
         <div>
 
-          <h4>{each.title}</h4>
+          <h4 className="guide-title" > {each.title} </h4>
           <audio controls src={each.audio.url} />
 
         </div>
@@ -65,23 +68,25 @@ class MeditateDetails extends Component {
     });
   };
 
-  displayDetails = () => {
+  displayDetails = () => { // DISPLAY DETAIL FUNCTION- PROGRAM, TITLE, NARRATORS, GUIDES
     let program = this.state.program;
 
     return (
       <div className="program">
-        <div className="title">
+        <div className="program-title">
           <h1>{program.title}</h1>
           <h2>{program.description}</h2>
         </div>
-        <div className="narrators">{this.getNarrators()}</div>
-        <div className="guides">{this.getGuides()}</div>
+
+        <div className="program-narrator"> {this.getNarrators()} </div>
+        <div className="program-guide"> {this.getGuides()} </div>
       </div>
+
     );
   };
 
   render() {
-    return <div>{this.state.program ? this.displayDetails() : ""}</div>;
+    return <div>{this.state.program ? this.displayDetails() : ""} </div>;
   }
 }
 
