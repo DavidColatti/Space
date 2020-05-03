12188594766325aa7d287835c3f5d1cc8c5803eb
gabrielacarr <briecardo@icloud.com>
2020-05-03 10:12:11 -0400

    push

--- src/components/Scenery/SceneryDetails.js
+++ src/components/Scenery/SceneryDetails.js
@@ -32,10 +32,12 @@ class SceneryDetails extends Component {
 			<div>
 				<NavBar />
 				<h1>{scene.title}</h1>
+				<div className="scene">
 				<video autoPlay loop contols="false" width="800px">
 					<source src={scene.video.url} type="video/mp4" />
 				</video>
 				<audio autoPlay loop src={scene.audio.url} />
+			</div>
 			</div>
 		);
 	};
