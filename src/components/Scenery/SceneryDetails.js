268e2a717a5b161d211ebf308e79e3b3b7075aa5
DavidColatti <davidcolatti@gmail.com>
2020-05-05 09:34:28 -0400

    clean up

--- src/components/Scenery/SceneryDetails.js
+++ src/components/Scenery/SceneryDetails.js
@@ -1,57 +1,55 @@
-import React, { Component } from "react";
-import scenes from "../../API/ScenesAPI.json";
-import NavBar from "../NavBar";
+import React, { Component } from 'react';
+import scenes from '../../API/ScenesAPI.json';
+import NavBar from '../NavBar';
 
 class SceneryDetails extends Component {
-  state = {
-    scene: null,
-  };
+	state = {
+		scene: null
+	};
 
-  componentDidMount() {
-    let scene;
-    const scenesCopy = [...scenes];
-    scenesCopy.find((each) => {
-      if (each.id === this.props.match.params.id) {
-        // console.log(each);
-        scene = each;
-        return true;
-      }
-      return false;
-    });
+	componentDidMount() {
+		let scene;
+		const scenesCopy = [ ...scenes ];
+		scenesCopy.find((each) => {
+			if (each.id === this.props.match.params.id) {
+				// console.log(each);
+				scene = each;
+				return true;
+			}
+			return false;
+		});
 
-    // console.log(program);
-    this.setState({
-      scene: scene,
-    });
-  }
+		// console.log(program);
+		this.setState({
+			scene: scene
+		});
+	}
 
-  displayDetails = () => {
-    let scene = this.state.scene;
+	displayDetails = () => {
+		let scene = this.state.scene;
 
-    return (
-      <div>
-        <NavBar />
+		return (
+			<div>
+				<NavBar />
 
-        <div className="scene">
-          <h1 className="scene-title"><strong><em>{scene.title}</em></strong></h1>
-          <video
-            className="video-img"
-            autoPlay
-            loop
-            contols="false"
-            width="800px"
-          >
-            <source src={scene.video.url} type="video/mp4" />
-          </video>
-          {/* <audio autoPlay loop src={scene.audio.url} /> */}
-        </div>
-      </div>
-    );
-  };
+				<div className="scene">
+					<h1 className="scene-title">
+						<strong>
+							<em>{scene.title}</em>
+						</strong>
+					</h1>
+					<video className="video-img" autoPlay loop contols="false" width="800px">
+						<source src={scene.video.url} type="video/mp4" />
+					</video>
+					<audio autoPlay loop src={scene.audio.url} />
+				</div>
+			</div>
+		);
+	};
 
-  render() {
-    return <div>{this.state.scene ? this.displayDetails() : ""}</div>;
-  }
+	render() {
+		return <div>{this.state.scene ? this.displayDetails() : ''}</div>;
+	}
 }
 
 export default SceneryDetails;
